bc936b31d41eae31e28fea46906bbaae3980b79b
Nicolas Ormeno <ni.ormeno@gmail.com>
2020-11-10 16:49:48 -0300

    Update error tests

--- test/error.test.ts
+++ test/error.test.ts
@@ -3,55 +3,56 @@ import { IError, IErrorResponse } from '../src/interfaces/IResponses';
 
 const apiVersion: string = "1.0";
 const context: string = "Error Tests";
+const gjsonResponse = new GjsonResponse(apiVersion, context);
 
 test('initialize', () => {
   const gjsonResponse = new GjsonResponse(apiVersion, context);
   expect(gjsonResponse).toBeDefined();
 });
 
-test('basic error', () => {
-  const gjsonResponse = new GjsonResponse(apiVersion, context);
+describe('Errors', () => {
+  test('Without details', () => {
+    const gjsonResponse = new GjsonResponse(apiVersion, context);
+    
+    const data: IError = {
+      code: "404",
+      message: "Not Found"
+    };
   
-  const data: IError = {
-    code: "404",
-    message: "Not Found"
-  };
-
-  const response: IErrorResponse = gjsonResponse.error(data);
+    const response: IErrorResponse = gjsonResponse.error(data);
+    
+    const expected: IErrorResponse = {
+      apiVersion,
+      context,
+      error: data
+    }
   
-  const expected: IErrorResponse = {
-    apiVersion,
-    context,
-    error: data
-  }
+    expect(response).toMatchObject(expected);
+  });
 
-  expect(response).toMatchObject(expected);
-});
-
-test('error with details', () => {
-  const gjsonResponse = new GjsonResponse(apiVersion, context);
+  test('With details', () => {
+    const data: IError = {
+      code: '404',
+      message: 'Not Found',
+      error: [{
+        domain: 'Calendar',
+        reason: 'ResourceNotFoundException',
+        message: 'File Not Found',
+        location: '',
+        locationType: '',
+        extendedHelper: 'http://url.to.more.details.example.com/',
+        sendReport: 'https://report.example.com/'
+      }]
+    };
   
-  const data: IError = {
-    code: '404',
-    message: 'Not Found',
-    error: [{
-      domain: 'Calendar',
-      reason: 'ResourceNotFoundException',
-      message: 'File Not Found',
-      location: '',
-      locationType: '',
-      extendedHelper: 'http://url.to.more.details.example.com/',
-      sendReport: 'https://report.example.com/'
-    }]
-  };
-
-  const response: IErrorResponse = gjsonResponse.error(data);
+    const response: IErrorResponse = gjsonResponse.error(data);
+    
+    const expected: IErrorResponse = {
+      apiVersion,
+      context,
+      error: data
+    }
   
-  const expected: IErrorResponse = {
-    apiVersion,
-    context,
-    error: data
-  }
-
-  expect(response).toMatchObject(expected);
+    expect(response).toEqual(expected);
+  });
 });
